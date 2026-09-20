27389e46cb0c50d498325c5af2cdbced962d223a
Kinetic Napier <u61819269@gmail.com>
2026-09-20 15:45:35 +0900

    fix: restore exact CSharpAtlas runtime source

--- src/content-loader.js
+++ src/content-loader.js
@@ -1,12 +1,39 @@
 import { localizeArticles } from './article-localization.js';
 
-export const CONTENT_CATEGORIES = ['items.json','exceptions.json','compiler-errors.json','compiler-warnings.json','concepts.json','code-recipes.json','logic-errors.json'];
+export const CONTENT_CATEGORIES = [
+  'items.json',
+  'exceptions.json',
+  'compiler-errors.json',
+  'compiler-warnings.json',
+  'concepts.json',
+  'code-recipes.json',
+  'logic-errors.json'
+];
 
 export async function loadLocalizedContent({ fetchJson, locale, fallbackLocale = 'ja' }) {
   if (typeof fetchJson !== 'function') throw new Error('fetchJson is required');
-  const baseGroups = await Promise.all(CONTENT_CATEGORIES.map(file => fetchJson(`./content/articles/${file}`)));
-  const fallbackGroups = await Promise.all(CONTENT_CATEGORIES.map(file => fetchJson(`./content/locales/${fallbackLocale}/${file}`)));
-  const requestedGroups = locale === fallbackLocale ? fallbackGroups : await Promise.all(CONTENT_CATEGORIES.map(file => fetchJson(`./content/locales/${locale}/${file}`)));
+
+  const baseGroups = await Promise.all(
+    CONTENT_CATEGORIES.map(file => fetchJson(`./content/articles/${file}`))
+  );
+  const fallbackGroups = await Promise.all(
+    CONTENT_CATEGORIES.map(file => fetchJson(`./content/locales/${fallbackLocale}/${file}`))
+  );
+  const requestedGroups = locale === fallbackLocale
+    ? fallbackGroups
+    : await Promise.all(
+      CONTENT_CATEGORIES.map(file => fetchJson(`./content/locales/${locale}/${file}`))
+    );
+
+  const baseArticles = baseGroups.flat();
   const mergeLocaleGroups = groups => Object.assign({}, ...groups);
-  return { articles: localizeArticles(baseGroups.flat(), { [fallbackLocale]: mergeLocaleGroups(fallbackGroups), [locale]: mergeLocaleGroups(requestedGroups) }, locale, fallbackLocale), requestedLocale: locale };
+  const localeMaps = {
+    [fallbackLocale]: mergeLocaleGroups(fallbackGroups),
+    [locale]: mergeLocaleGroups(requestedGroups)
+  };
+
+  return {
+    articles: localizeArticles(baseArticles, localeMaps, locale, fallbackLocale),
+    requestedLocale: locale
+  };
 }
